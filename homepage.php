f207b727d2f90c8f8a9b1e26c1ce73b4529f0e91
ripsime-arakelyan <6hdjpxfjpq@privaterelay.appleid.com>
2025-11-22 13:31:54 +0100

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="hp-style.css">
+  <title>Home Page</title>
+</head>
+<body>
+
+
+<div class="header">
+<header>
+    <div id="logo">DEVRRI</div>
+    <nav>
+        <ul>
+            <li><a href="homepage.php">Home</a></li>
+            <li><a href="zimmerInfo.php">Zimmern</a></li>
+            <li><a href="uberuns.php">Über uns</a></li>
+            <li><a href="kunden.php">Kunden</a></li>
+        </ul>
+    </nav>
+</header>  
+</div>   
+
+<div class="hero content">
+    <h1>DEVRRI HOTEL</h1>
+     <div class="bttns">
+
+                <button><a href="zimmerInfo.php"> Zimmern ansehen</a> </button> 
+                <button> <a href="#">Buchung machen</a></button>
+    </div>
+</div>
+
+<section class="info-block">
+  <div class="hp-uberuns">
+    <div class="teil1">
+      <img src="images/hp-pic1.png" alt="">
+    </div>
+
+    <div class="teil2">
+        <p class="kicker">kürz über uns:</p>
+        <h2>Willkommen im DEVRRI Hotel</h2>
+        <p>Willkommen im Hotel Devrri – einem Ort, an dem Gastfreundschaft, Komfort und Natur zu einem unvergesslichen Erlebnis verschmelzen. Genießen Sie erstklassigen Service und die Ruhe einer einzigartigen Umgebung.</p>
+        <a href="uberuns.php"><button>mehr ></button></a>
+      </div>
+  </div>
+</section>
+
+
+
+
+<footer>
+  <div class="blocks container">
+    <div>
+      <h4>Events:</h4>
+      <ul>
+        <li>Mo 22.10.25 - Geburtstagsparty des Hotels</li>
+        <li>Di 23.10.25 - Konzert mit live Musik</li>
+      </ul>
+    </div>
+    <div>
+      <h4>Über uns</h4>
+      <ul>
+        <li><a href="uberuns.php">Gründung</a></li>
+        <li><a href="uberuns.php#ausbildung">Ausbildung</a></li>
+      </ul>
+    </div>
+    <div>
+      <h4>Kontakt</h4>
+      <span>📍 Musterstraße 12, 22000 Berlin</span>
+      <p> ✉️ E-Mail: info@devrri.de</p>
+      <p> 📞 tel: +4917551027090</p>
+    </div>
+  </div>
+  <hr>
+  <p>© 2025 DEVRRI Berlin – Alle Rechte vorbehalten.</p>
+</footer>
+
+</body>
+</html>
